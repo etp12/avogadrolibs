6a5f41c0206bc21b9e90a093e978c29b79e665bb
Ethan Pavolik <etp12@pitt.edu>
2017-04-23 20:22:03 -0400

    Fine-tune vrml camera position

--- avogadro/rendering/vrmlvisitor.cpp
+++ avogadro/rendering/vrmlvisitor.cpp
@@ -38,13 +38,13 @@ using std::ofstream;
 namespace {
 ostream& operator<<(ostream& os, const Vector3f& v)
 {
-  os << v[0] << ", " << v[1] << ", " << v[2];
+  os << v[0] << " " << v[1] << " " << v[2];
   return os;
 }
 
 ostream& operator<<(ostream& os, const Vector3ub& color)
 {
-  os << color[0] / 255.0f << ", " << color[1] / 255.0f << ", "
+  os << color[0] / 255.0f << "\t" << color[1] / 255.0f << "\t"
      << color[2] / 255.0f;
   return os;
 }
@@ -62,7 +62,7 @@ VRMLVisitor::~VRMLVisitor()
 
 void VRMLVisitor::begin()
 {
-	/*
+
   // Initialise the VRML scene
   Vector3f cameraT = -(m_camera.modelView().linear().adjoint() *
                        m_camera.modelView().translation());
@@ -79,39 +79,16 @@ void VRMLVisitor::begin()
     huge * (m_camera.modelView().linear().adjoint() * Vector3f(0, 1, 0));
 
   // Output the POV-Ray initialisation code
+  // orientation should be set
+  // http://cgvr.informatik.uni-bremen.de/teaching/vr_literatur/Calculating%20VRML%20Viewpoints.html
   ostringstream str;
-  str << "global_settings {\n"
-      << "\tambient_light rgb <" << m_ambientColor << ">\n"
-      << "\tmax_trace_level 15\n}\n\n"
-      << "background { color rgb <" << m_backgroundColor << "> }\n\n"
-      << "camera {\n"
-      << "\tperspective\n"
-      << "\tlocation <" << cameraT.x() << ", " << cameraT.y() << ", "
-      << cameraT.z() << ">\n"
-      << "\tangle 70\n"
-      << "\tup <" << cameraY.x() << ", " << cameraY.y() << ", " << cameraY.z()
-      << ">\n"
-      << "\tright <" << cameraX.x() << ", " << cameraX.y() << ", "
-      << cameraX.z() << "> * " << m_aspectRatio << '\n'
-      << "\tdirection <" << cameraZ.x() << ", " << cameraZ.y() << ", "
-      << cameraZ.z() << "> }\n\n"
-
-      << "light_source {\n"
-      << "\t<" << light0pos[0] << ", " << light0pos[1] << ", " << light0pos[2]
-      << ">\n"
-      << "\tcolor rgb <1.0, 1.0, 1.0>\n"
-      << "\tfade_distance " << 2 * huge << '\n'
-      << "\tfade_power 0\n"
-      << "\tparallel\n"
-      << "\tpoint_at <" << -light0pos[0] << ", " << -light0pos[1] << ", "
-      << -light0pos[2] << ">\n"
-      << "}\n\n"
-
-      << "#default {\n\tfinish {ambient .8 diffuse 1 specular 1 roughness .005 "
-         "metallic 0.5}\n}\n\n";
+  str << "#VRML V2.0 utf8\n"
+      << "DEF DefaultView Viewpoint {\n"
+      << "position " << cameraT << " \n"
+      << "fieldOfView 0.785398\n}\n";
 
   m_sceneData = str.str();
-	*/
+
 }
 
 string VRMLVisitor::end()
@@ -130,18 +107,16 @@ void VRMLVisitor::visit(SphereGeometry& geometry)
   for (size_t i = 0; i < geometry.spheres().size(); ++i) {
     Rendering::SphereColor s = geometry.spheres()[i];
 
-		str << "Transform {\n"
-			<< "\ttranslation\t" << s.center[0] << "\t" << s.center[1] << "\t" << s.center[2]
-			<< "\n\tchildren Shape {\n"
-			<< "\t\tgeometry Sphere {\n\t\t\tradius\t" << s.radius << "\n\t\t}\n"
-			<< "\t\tappearance Appearance {\n"
-			<< "\t\t\tmaterial Material {\n"
-			<< "\t\t\t\tdiffuseColor\t" << s.color[0] << "\t" << s.color[1]
-			<< "\t" << s.color[2] << "\n\t\t\t}\n\t\t}\n\t}\n}\n";
+    str << "Transform {\n"
+      << "\ttranslation\t" << s.center[0] << "\t" << s.center[1] << "\t" << s.center[2]
+      << "\n\tchildren Shape {\n"
+      << "\t\tgeometry Sphere {\n\t\t\tradius\t" << s.radius << "\n\t\t}\n"
+      << "\t\tappearance Appearance {\n"
+      << "\t\t\tmaterial Material {\n"
+      << "\t\t\t\tdiffuseColor\t" << s.color << "\n\t\t\t}\n\t\t}\n\t}\n}\n";
 
   }
   m_sceneData += str.str();
-
 
 }
 
@@ -156,64 +131,59 @@ void VRMLVisitor::visit(CylinderGeometry& geometry)
   for (size_t i = 0; i < geometry.cylinders().size(); ++i) {
     Rendering::CylinderColor c = geometry.cylinders()[i];
 
-		double scale = 1.0;
-		double x1, x2, y1, y2, z1, z2;
-		x1 = c.end1[0];
-		x2 = c.end2[0];
-		y1 = c.end1[1];
-		y2 = c.end2[1];
-		z1 = c.end1[2];
-		z2 = c.end2[2];
+    //double scale = 1.0;
+    double x1, x2, y1, y2, z1, z2;
+    x1 = c.end1[0];
+    x2 = c.end2[0];
+    y1 = c.end1[1];
+    y2 = c.end2[1];
+    z1 = c.end1[2];
+    z2 = c.end2[2];
 
-		double dx = x2 - x1;
-		double dy = y2 - y1;
-		double dz = z2 - z1;
+    double dx = x2 - x1;
+    double dy = y2 - y1;
+    double dz = z2 - z1;
 
-		double length = sqrt(dx*dx + dy*dy + dz*dz);
-		double tx = dx / 2 + x1;
-		double ty = dy / 2 + y1;
-		double tz = dz / 2 + z1;
+    double length = sqrt(dx*dx + dy*dy + dz*dz);
+    double tx = dx / 2 + x1;
+    double ty = dy / 2 + y1;
+    double tz = dz / 2 + z1;
 
-		dx = dx / length;
-		dy = dy / length;
-		dz = dz / length;
+    dx = dx / length;
+    dy = dy / length;
+    dz = dz / length;
 
-		double ax, ay, az, angle;
+    double ax, ay, az, angle;
 
-		if (dy > 0.999) {
-			ax = 1.0;
-			ay = 0.0;
-			az = 0.0;
-			angle = 0.0;
-		}
-		else if (dy < -0.999) {
-			ax = 1.0;
-			ay = 0.0;
-			az = 0.0;
-			angle = 3.14159265359;
-		}
-		else {
-			ax = dz;
-			ay = 0.0;
-			az = dx * -1.0;
-			angle = acos(dy);
-		}
-		length = length / 2.0;
+    if (dy > 0.999) {
+      ax = 1.0;
+      ay = 0.0;
+      az = 0.0;
+      angle = 0.0;
+    }
+    else if (dy < -0.999) {
+      ax = 1.0;
+      ay = 0.0;
+      az = 0.0;
+      angle = 3.14159265359;
+    }
+    else {
+      ax = dz;
+      ay = 0.0;
+      az = dx * -1.0;
+      angle = acos(dy);
+    }
+    length = length / 2.0;
 
-//		if ((radius*scale) < this->thinnestCyl) //keep track of the thinnest cylinder
-//			this->thinnestCyl = radius*this->scale*2; //diameter
-
-		str << "Transform {\n"
-			<< "\ttranslation\t" << tx * scale << "\t" << ty * scale << "\t" << tz * scale
-			<< "\n\tscale " << " 1 " << scale << " 1"
-			<< "\n\trotation " << ax << " " << ay << " " << az << " " << angle
-			<< "\n\tchildren Shape {\n"
-			<< "\t\tgeometry Cylinder {\n\t\t\tradius\t" << c.radius * scale << "\n\t\t}\n"
-			<< "\t\tappearance Appearance {\n"
-			<< "\t\t\tmaterial Material {\n"
-			<< "\t\t\t\tdiffuseColor\t" << c.color[0] << "\t" << c.color[1]
-			<< "\t" << c.color[2] << "\n\t\t\t}\n\t\t}\n\t}\n}\n";
-
+    str << "Transform {\n"
+      << "\ttranslation\t" << tx << "\t" << ty  << "\t" << tz
+      << "\n\tscale " << " 1 " << length << " 1"
+      << "\n\trotation " << ax << " " << ay << " " << az << " " << angle
+      << "\n\tchildren Shape {\n"
+      << "\t\tgeometry Cylinder {\n\t\t\tradius\t" << c.radius << "\n\t\t}\n"
+      << "\t\tappearance Appearance {\n"
+      << "\t\t\tmaterial Material {\n"
+      << "\t\t\t\tdiffuseColor\t" << c.color << "\n\t\t\t}\n\t\t}\n\t}\n}\n";
   }
   m_sceneData += str.str();
 }
